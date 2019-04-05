052912fd5c1fb01dd52251cffa092ec8842c63ab
Mike Eve <ungrim97@gmail.com>
2019-04-05 15:59:48 +0100

    Fix: continue to try and fix race for coverage file

--- bin/docker/run_tests.sh
+++ bin/docker/run_tests.sh
@@ -5,6 +5,7 @@ npm -s test
 npm run -s test:integration
 
 echo "Running coverage. Report stored in coverage.lcov"
-npm run -s coverage -- --reporter=text-lcov > coverage.lcov
+exec npm run -s coverage -- --reporter=text-lcov > coverage.lcov
+
 wait
 
