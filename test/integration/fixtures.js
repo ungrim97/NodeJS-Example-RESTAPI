ed8e95559c3d8be9e0f586c69f7c6443f8138284
Mike Eve <ungrim97@gmail.com>
2019-04-07 12:47:05 +0100

    Added fixture population script

---
+++ test/integration/fixtures.js
@@ -0,0 +1,13 @@
+const fixtureData = require('../../database/fixtures/messagedata.fixture');
+
+module.exports = {
+  populate(store) {
+    return fixtureData(store.messageStore);
+  },
+  depopulate(store) {
+    return store.messageStore.models.message.destroy({
+      where: {},
+      truncate: true
+    });
+  }
+};
